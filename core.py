f950df822fc03e5bcd97409b29d105d118b93a6c
Jeremy Truvillion <jeremytruvillion@proofme.ai>
2025-08-03 17:57:50 -0700

    Add root route to serve homepage

--- core.py
+++ core.py
@@ -105,5 +105,9 @@ Metadata:
 def serve_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route('/')
+def index():
+    return send_from_directory('templates', 'index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
